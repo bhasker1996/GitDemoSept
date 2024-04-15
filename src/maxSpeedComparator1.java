f0cd8bef4c397c7f5d84f611ecef78fe2d6ff88c
bhasker1996 <bhasker.medathati@gmail.com>
2024-04-15 23:06:34 +0530

    Adding sout in the maxspeed comparator class

--- src/maxSpeedComparator1.java
+++ src/maxSpeedComparator1.java
@@ -3,6 +3,7 @@ import java.util.Comparator;
 public class maxSpeedComparator1 implements Comparator<Car> {
     @Override
     public int compare(Car o1, Car o2) {
+        System.out.println("");
         return o1.getMaxSpeed() - o2.getMaxSpeed();
     }
 }
